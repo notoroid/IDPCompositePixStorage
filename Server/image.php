03178cadf5f8b04a85c4c2e5fe4e109118e894de
notoroid <noto@irimasu.com>
2015-12-02 17:07:33 +0900

    support pdf file.

--- Server/image.php
+++ Server/image.php
@@ -1,11 +1,18 @@
 <?php
 define('BASE_DIRECTORY', "./" );
 
-$us_path = $_POST['path'];
+$us_path = $_REQUEST['path'];
 $result = preg_match('/(\.\.\/|\/|\.\.\\\\)/', $us_path) ? TRUE : FALSE;
 $s_path = $result == TRUE ? (BASE_DIRECTORY . $us_path) : "";
 
-header('Content-type: image/jpeg');
+$pathInformation = pathinfo($s_path);
+
+// アクセス可能なMINEを設定
+$supportMINEs = array('gif'=>'image/gif','jpg'=>'image/jpeg','jpeg'=>'image/jpeg','png'=>'image/png','pdf'=>'application/pdf');
+
+$content_type = $supportMINEs[$pathInformation['extension']];
+
+header('Content-type: ' . $content_type );
 readfile(/*$s_path*/ BASE_DIRECTORY . $us_path );
 
 ?>
